76ef276b62e07438911cf368e284e3fb9db894f0
Mira <mira.shenron@gmail.com>
2020-10-28 17:43:48 +0200

    commit

--- js/controller.js
+++ js/controller.js
@@ -22,76 +22,71 @@ window.onload = () => {
         .catch(err => {
             console.log('err!!!', err);
         })
+
+    document.querySelector('.btn-my-location').addEventListener('click', (ev) => {
+        console.log('Aha!', ev.target);
+        getPosition().then(res => {
+            panTo(res.coords.latitude, res.coords.longitude);
+        });
+    })
 }
-
-document.querySelector('.btn-my-location').addEventListener('click', (ev) => {
-    console.log('Aha!', ev.target);
-
-    navigator.geolocation.getCurrentPosition((ev) => {
-        var currLoc = {
-            lat:ev.coords.latitude, 
-            lng:ev.coords.longitude
-        };
-        panTo(ev.coords.latitude, ev.coords.longitude);
-    });
-})
 
 
 export function initMap(lat = 32.0749831, lng = 34.9120554) {
-    console.log('InitMap');
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available');
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
+        console.log('InitMap');
+        return _connectGoogleApi()
+            .then(() => {
+                console.log('google available');
+                gMap = new google.maps.Map(
+                    document.querySelector('#map'), {
                     center: { lat, lng },
                     zoom: 15
                 })
-                gMap.addListener('click', function(event){
+                gMap.addListener('click', function (event) {
                     var myLatlng = {
                         lat: event.latLng.lat(),
                         lng: event.latLng.lng()
                     }
-                    // var name = prompt('Enter place name:');
+                    
                     // addPlace(name, myLatlng); ??should add this place to locations table
                     gMap.setCenter(myLatlng);
                 })
-            console.log('Map!', gMap);
+                console.log('Map!', gMap);
+            })
+    }
+
+    function addMarker(loc) {
+        var marker = new google.maps.Marker({
+            position: loc,
+            map: gMap,
+            title: 'Hello World!'
+        });
+        return marker;
+    }
+
+    function panTo(lat, lng) {
+        var laLatLng = new google.maps.LatLng(lat, lng);
+        gMap.panTo(laLatLng);
+    }
+
+    function getPosition() {
+        console.log('Getting Pos');
+
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
         })
-}
-
-function addMarker(loc) {
-    var marker = new google.maps.Marker({
-        position: loc,
-        map: gMap,
-        title: 'Hello World!'
-    });
-    return marker;
-}
-
-function panTo(lat, lng) {
-    var laLatLng = new google.maps.LatLng(lat, lng);
-    gMap.panTo(laLatLng);
-}
-
-function getPosition() {
-    console.log('Getting Pos');
-
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject)
-    })
-}
+    }
 
 
-function _connectGoogleApi() {
-    if (window.google) return Promise.resolve()
-    var elGoogleApi = document.createElement('script');
-    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBCHE8wlLLHaFn9_WPzvZ7R0yrLsZDDRvA`;
-    elGoogleApi.async = true;
-    document.body.append(elGoogleApi);
+    function _connectGoogleApi() {
+        if (window.google) return Promise.resolve()
+        var elGoogleApi = document.createElement('script');
+        elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBCHE8wlLLHaFn9_WPzvZ7R0yrLsZDDRvA`;
+        elGoogleApi.async = true;
+        document.body.append(elGoogleApi);
 
-    return new Promise((resolve, reject) => {
-        elGoogleApi.onload = resolve;
-        elGoogleApi.onerror = () => reject('Google script failed to load')
-    })
-}
+        return new Promise((resolve, reject) => {
+            elGoogleApi.onload = resolve;
+            elGoogleApi.onerror = () => reject('Google script failed to load')
+        })
+    }
